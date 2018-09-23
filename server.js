6884278f6ed86fa9bd74b0ba1c17e3cc98bf2855
Hugh Rawlinson <hugh@spotify.com>
2018-09-23 20:44:40 +0200

    More work

--- server.js
+++ server.js
@@ -1,97 +1,49 @@
 #!/usr/bin/env node
 
+global.DB_URI = "";
+
 (function(){
+  // node-getopt oneline example.
+  opt = require('node-getopt').create([
+    ['d', '='                    ,'database connect string'],
+    ['p', '='                    ,'port'],
+    ['h', 'help'                 ,'display this help']
+  ])              // create Getopt instance
+  .bindHelp()     // bind option 'help' to default action
+  .parseSystem(); // parse command line
+
   var Hapi = require('hapi');
-  var MongoClient = require('mongodb').MongoClient;
-  var ObjectID = require('mongodb').ObjectID;
-
-
-  // Connection URL
-  if(!process.env.MONGO_URI || !process.env.JAMS_PORT){
-    console.error("Environmental config not properly specified");
-    process.exit();
-  }
 
   var server = new Hapi.Server();
-  server.connection({ port: process.env.JAMS_PORT });
+  server.connection({ port: opt.options.p });
+  DB_URI = opt.options.d;
 
   // Use connect method to connect to the Server
-  MongoClient.connect(process.env.MONGO_URI, function(err, db) {
-    if(err){
-      console.error(err);
-      process.exit();
-    }
-
-    var jams = db.collection('jams');
-
-    server.route({
-      method: 'PUT',
-      path: '/jams',
-      handler: function (request, reply) {
-        var payload = {
-          data: request.payload
-        };
-        jams.insertOne(payload,function(err,result){
-          if(err){
-            console.error('failed to insert jam '+request.body.file_metadata.identifiers.filename);
-            reply({status:'500'});
-          }
-          else{
-            var r = JSON.parse(result);
-            return reply({
-              id:r.electionId,
-            });
-          }
-        });
-      }
-    });
-
-    server.route({
-      method: 'GET',
-      path: '/jams/{id}',
-      handler: function(request, reply){
-        // TODO: Validate id, throw 400
-        jams.findOne({_id:ObjectID(request.params.id)},function(err,result){
-          if(err){
-            // TODO: Check if this is meant to be a 404//5XX
-            reply({status:500});
-          }
-          else{
-            if(!result){
-              reply({status:404});
-            }
-            reply(result.data);
-          }
-        });
-      }
-    });
-
-    server.route({
-      method: 'GET',
-      path: '/jams',
-      handler: function(request, reply){
-        jams.find({}).limit(30).toArray(function(err,result){
-          if(err){
-            reply({status:500});
-          }
-          else{
-            var ids = [];
-            result.map(function(val){
-              ids.push(val._id);
-            });
-            reply({jams:ids});
-          }
-        });
-      }
-    });
-
-    server.start(function () {
-      console.log('Server running at:', server.info.uri);
-    });
-
-
-    process.on('exit',function(){
-      db.close();
-    });
+  server.route({
+    method: 'PUT',
+    path: '/jams',
+    handler: require('./lib/handlers/put_jams')
   });
+
+  server.route({
+    method: 'GET',
+    path: '/jams/{id}',
+    handler: require('./lib/handlers/get_individual_jams')
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/jams',
+    handler: require('./lib/handlers/get_jams_id_list')
+  });
+
+  server.route({
+    method:'GET',
+    path: '/',
+    handler: function(request, reply){
+      reply("yes hello");
+    }
+  })
+
+  server.start(function(){console.log('server started');});
 })();
